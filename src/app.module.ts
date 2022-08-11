cf1f3a3557ab7ecee1a06d4dd12cd1d9fed7819e
Pavel <artpavel@ukr.net>
2022-08-11 14:30:17 +0300

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from 'ormconfig'
+import { AppDataSource } from 'ormconfig';
 import { PersonModule } from './person/person.module';
 import { CarModule } from './car/car.module';
 
@@ -12,7 +12,7 @@ import { CarModule } from './car/car.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(AppDataSource.options),
     PersonModule,
-    CarModule
+    CarModule,
   ],
   controllers: [AppController],
   providers: [AppService],
